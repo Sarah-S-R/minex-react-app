b30cadd0e8432d80ca366c93f357e700cf3978e0
Clark Oake <clarkoake13@gmail.com>
2024-06-05 19:44:41 -0230

    add ProtectedRoutes and basic Auth Infrastructure
    Created an AuthProvider Context-based component that wraps the application with the current user's details (only uses the username / email rn but can be extended to include whatever properties need to be included)
    
    Also added the createBrowserRouter hook from react-router-dom to wrap the app with a RouterProvider that also handles Protected Routes to ensure non-auth'ed users can't see content intended for Auth'ed users only
    
    tested all described functionality and ensured it was working as expected

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Home from './Home';
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
@@ -17,31 +20,109 @@ import './styles/dashstyles.css';
 import './styles/Home.css';
 import './styles/index.css';
 
+import AuthProvider from './hooks/AuthProvider';
+import ProtectedRoute from './components/Auth/ProtectedRoute';
 
-// TODO: Look into ProtectedRoutes withn react router dom to render a different public page vs auth'ed page
+// TODO: Check out your ESLInt config, package imports seem to be broken
 
+// TODO: Routing -> ensure all app routing uses react-router-dom patterns and hooks
+// TODO: Routing -> set up Protected Routes for working with Auth
+
+// TODO: Styling -> Ensure CSS styles are applied as expected in all components & refactor as needed
+// TODO: Styling -> Revisit core layout of application, ensure components are structured as expeted across views
+
+// TODO: Data Model -> Generate data structures for each data set required in JSON format as a sample
+// TODO: Data Model -> Decide on a State Management system & identify key handler methods
+// TODO: Data Model -> Initialize some app state to populate views generically for testing
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/forgot_password",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/profile",
+    element: <ProtectedRoute 
+                childRoute={<Profile />}
+              />,
+  },
+  {
+    path: "/dashboard",
+    element: <ProtectedRoute 
+                childRoute={<Dashboard />}
+              />,
+  },
+  {
+    path: "/locations",
+    element: <ProtectedRoute 
+                childRoute={<Locations />}
+              />,
+  },
+  {
+    path: "/clients",
+    element: <ProtectedRoute 
+                childRoute={<Clients />}
+              />,
+  },
+  {
+    path: "/projects",
+    element: <ProtectedRoute 
+                childRoute={<Projects />}
+              />,
+  },
+  {
+    path: "/properties",
+    element: <ProtectedRoute 
+                childRoute={<Properties />}
+              />,
+  }
+]);
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Sidebar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/locations" element={<Locations />} />
-          <Route path="/clients" element={<Clients />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/properties" element={<Properties />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <AuthProvider>
+      {/* <Router> */}
+        {/* <div className="App"> */}
+          {/* <Header /> */}
+          {/* <Sidebar /> */}
+          <RouterProvider router={router} />
+          {/* <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/locations" element={<Locations />} />
+            <Route path="/clients" element={<Clients />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/properties" element={<Properties />} />
+          </Routes> */}
+          {/* <Footer /> */}
+        {/* </div> */}
+      {/* </Router> */}
+    </AuthProvider>
   );
 }
 
